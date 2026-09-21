46254fc8f2ce3a92d93b42d926d6b33ab914772d
emilialeonova29-ux <emilialeonova29@gmail.com>
2026-09-21 22:08:50 +0300

    Create mini-sync-config.js

---
+++ mini-sync-config.js
@@ -0,0 +1,6 @@
+window.MINI_SYNC_CONFIG = {
+  supabaseUrl: 'https://acfuzhmqjdaypnhfabec.supabase.co',
+  supabasePublishableKey: 'sb_publishable_RXV49_9pWkIABvqqTEU11g_KzjQPmSC',
+  defaultResourceId: 'interactive-mini-sentence-characteristics-v1',
+  requireWatch: true
+};
